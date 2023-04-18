4a59a9ca719c1b8d2cd781fb7a43b4cf88a12459
Duynava <128036893+Duynava@users.noreply.github.com>
2023-04-18 17:32:56 +0700

    bai_thi_nang_caoJV230313

---
+++ baithi/app.js
@@ -0,0 +1,29 @@
+let student=[
+{
+    id: 1,
+    name: "rikkei",
+    email: "rikkei@gmail",
+    phone: "0123456789",
+    assignedTo:"ha noi",
+    gender:"nam"
+},
+{
+    id: 2,
+    name: "acdemy",
+    email: "acdemy@gmail",
+    phone: "09876543221",
+    assignedTo:"hcm",
+    gender:"nu"
+},
+{
+    id: 3,
+    name: "kho qua",
+    email: "acdemy@gmail",
+    phone: "09876543221",
+    assignedTo:"tay thien",
+    gender:"nu"
+}
+
+]
+let form=document.getElementById("main-form")
+
